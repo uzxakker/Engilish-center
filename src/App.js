bbfdba3f223651c2f666f61c69e4605ca171a5bc
Obidjonov Abdulloh <suprablack12@gmail.com>
2023-01-30 15:48:17 +0500

    engilish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { GlobalStyle, Div } from "./App-styled";
+import About from "./components/About";
+import Categorise from "./components/categorise";
+import Course from "./components/Course";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Leave from "./components/Leave";
+import Online from "./components/Online";
+import Questions from "./components/Questions";
+import Result from "./components/Result";
+import System from "./components/Sytem";
+import Tariffs from "./components/Tariffs";
+import Teacher from "./components/Teacher";
+import Teachers from "./components/teachers";
+import Values from "./components/Values";
+import Watch from "./components/Watch";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Div>
+        <GlobalStyle />
+        <Header />
+        <System />
+        <Course />
+        <Watch />
+        {/* <Categorise /> */}
+        <Values />
+        <Online />
+        <Result />
+        <About />
+        <Teachers />
+        <Tariffs />
+        <Teacher/>
+        <Questions />
+        <Leave />
+        <Footer />
+      </Div>
+    </>
   );
 }
 
